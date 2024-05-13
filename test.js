7256f2f238d39518607c1cf0bd0c950c33251a0f
john thuo <john.thuo@technobrainbpo.com>
2024-05-13 23:50:36 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+const val1 = Boolean(false);
+const val2 = Number(100);
+const val3 = BigInt(1e9);
+const val4 = String("john");
+
+console.log(val1,val2,val3,val4);
+
+const val5 = 10;
+const val6 = 35;
+let sum2;
+
+sum2 = val5 + val6;
+
+console.log(sum2);
